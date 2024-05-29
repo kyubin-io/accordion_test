5d99ec7b5452ca4787941ac27f9fce296dbf2499
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-05-29 15:21:08 +0900

    reusability with the children

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem key={i} title={el.title} text={el.text} num={i} />
+        <AccordionItem
+          key={i}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
-export default function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function AccordionItem({
+  num,
+  title,
+  curOpen,
+  onOpen,
+  children,
+}) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -12,7 +18,7 @@ export default function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
